a704e94b45f47c6e446dd69fd10565610221b31c
Adrienne Tchamgoue <adrienne.tchamgoue@gmail.com>
2020-04-05 21:50:53 +0100

    Ajout des migrations users et groups

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigIncrements('id');
+            $table->integer('group_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->enum('gender', ['F', 'M']);
+            $table->string('phone');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('avatar');
+            $table->boolean('active');
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
