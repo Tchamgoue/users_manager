4e5ae8ea7da13d4c7cde4f44171869f3cfa9774a
Adrienne Tchamgoue <adrienne.tchamgoue@gmail.com>
2020-04-05 22:25:13 +0100

    Mise à jour des routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/groups', 'GroupController@index');
+Route::get('/groups/{id}', 'GroupController@show');
+Route::post('/groups', 'GroupController@store');
+Route::post('/groups/{id}', 'GroupController@update');
+Route::delete('groups/{id}', 'GroupController@destroy');
+
+Route::get('groups/{id}/users', 'GroupController@getUsers');
+
+Route::get('/users', 'UserController@index');
+Route::get('/users/{id}', 'UserController@show');
+Route::get('/users/{id}/group', 'UserController@group');
+Route::post('/users', 'UserController@store');
+Route::post('/users/{id}', 'UserController@update');
+Route::delete('users/{id}', 'UserController@destroy');
